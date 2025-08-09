17c0e090b02f888973a4947181762cf8221f922c
Mohammed Asad Khan <114600171+MAK-2723@users.noreply.github.com>
2025-08-09 17:48:05 +0530

    Update websocket.py

--- backend/app/websocket.py
+++ backend/app/websocket.py
@@ -1,13 +1,19 @@
+from typing import List
+from fastapi import WebSocket
+
 class ConnectionManager:
     def _init_(self):
-        self.active_connections=[]
+        self.active_connections: List[WebSocket]=[]
 
-    async def connect(self,websocket):
+    async def connect(self,websocket: WebSocket):
         await websocket.accept()
         self.active_connections.append(websocket)
     
     def disconnect(self,websocket):
         self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
 
     async def broadcast(self,message: str):
         for connection in self.active_connections:
