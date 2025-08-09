de7c8a60362b3f3c221f8c97b70659297160375c
Mohammed Asad Khan <114600171+MAK-2723@users.noreply.github.com>
2025-08-09 11:32:44 +0530

    Create main.py

---
+++ backend/app/main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.exceptions import RequestValidationError, HTTPException
+from app.middleware import http_exception_handler, validation_exception_handler
+from app import database, crud, websocket
+import uvicorn
+
+app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_exception_handler(HTTPException, http_exception_handler)
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
+
+@app.post("/webhook/")
+async def receive_payload(payload: dict):
+    await crud.process_payload(payload)
+    await websocket.manager.broadcast("new_data")
+    return {"status":"processed"}
+
+@app.get("/messages/{wa_id}")
+async def get_messages(was_id: str):
+    return await crud.get_messages_by_user(wa_id)
+
+@app.get("/conversations")
+async def get_conversations():
+    return await crud.get_all_conversations()
+
+@app.post("/send")
+async def send_message(data: dict):
+    await crud.insert_message(data)
+    await websocket.manager.broadcast("new_data")
+    return {"status":"saved"}
+
+@app.websocket("/ws")
+async def websocket_endpoint(ws: WebSocket):
+    await websocket.manager.connect(ws)
+    try:
+        while True:
+            await ws.receive_text()
+    except WebSocketDisconnect:
+        websocket.manager.disconnect(ws)
+
+if __name__=="__main__":
+    uvicorn.run("app.main:app",host="0.0.0.0",port=8000)
