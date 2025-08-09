a96920d41480d421a14e4d4c7090912d61ff323e
Mohammed Asad Khan <114600171+MAK-2723@users.noreply.github.com>
2025-08-09 11:39:32 +0530

    Create api.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,19 @@
+const BASE = "https://localhost:8000";
+
+export async function fetchConversations() {
+    const res=await fetch(`${BASE}/conversations`);
+    return res.json();
+}
+
+export async function fetchMessages(wa_id) {
+    const res=await fetch(`${BASE}/messages/${wa_id}`);
+    return res.json();
+}
+
+export async function sendMessage(data) {
+    await fetch(`${BASE}/send`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    });
+}
