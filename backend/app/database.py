b881513ff71666f280c65555994f76bbbbb8c4f2
Mohammed Asad Khan <114600171+MAK-2723@users.noreply.github.com>
2025-08-10 13:32:25 +0530

    Update database.py
    Added logger

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,16 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from os import getenv
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
+logger=logging.getLogger("database")
 
 MONGO_URI=getenv("MONGO_URI")
+if not MONGO_URI:
+  logger.error("MONGO_URI not set in env")
+  raise RuntimeError("MONGO_URI not set")
+  
 client=AsyncIOMotorClient(MONGO_URI)
 db=client.whatsapp
 
