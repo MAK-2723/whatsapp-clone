1283e47c186bf2f535e7e9eb734d172d416d953c
Mohammed Asad Khan <114600171+MAK-2723@users.noreply.github.com>
2025-08-10 20:07:37 +0530

    Update ChatWindow.js

--- frontend/src/components/ChatWindow.js
+++ frontend/src/components/ChatWindow.js
@@ -3,26 +3,27 @@ import { fetchMessages } from '../services/api';
 import MessageBubble from "./MessageBubble";
 import SendMessageForm from "./SendMessage";
 
-export default function ChatWindow({wa_id}) {
+export default function ChatWindow({conversation_id}) {
     const [messages, setMessages]= useState([]);
 
     const load= async() => {
-        const data = await fetchMessages(wa_id);
+        const data = await fetchMessages(conversation_id);
         setMessages(data);
     };
     
     useEffect(() => {
         load();
-    }, [wa_id]);
+    }, [conversation_id]);
 
     return (
         <div className="w-2/3 flex flex-col bg-chat-pattern">
             <div className="flex-1 p-4 overflow-y-auto space-y-3">
                 {messages.map((msg, index) => {
-                    <MessageBubble key={index} message={msg} isSender={msg.name === 'You'} />
+                    <MessageBubble key={msg._id || index} message={msg} isSender={msg.name === 'You'} />
                 })}
             </div>
-            <SendMessageForm wa_id={wa_id} reload={load}/>
+            <SendMessageForm conversation_id={conversation_id} reload={load}/>
         </div>
     );
 }
+
