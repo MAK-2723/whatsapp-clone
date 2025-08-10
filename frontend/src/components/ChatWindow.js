9fc8df76e554c7674167b3405106901ed6d92d31
Mohammed Asad Khan <114600171+MAK-2723@users.noreply.github.com>
2025-08-10 18:59:49 +0530

    Update ChatWindow.js

--- frontend/src/components/ChatWindow.js
+++ frontend/src/components/ChatWindow.js
@@ -6,11 +6,12 @@ import SendMessageForm from "./SendMessage";
 export default function ChatWindow({wa_id}) {
     const [messages, setMessages]= useState([]);
 
+    const load= async() => {
+        const data = await fetchMessages(wa_id);
+        setMessages(data);
+    };
+    
     useEffect(() => {
-        async function load() {
-            const data = await fetchMessages(wa_id);
-            setMessages(data);
-        }
         load();
     }, [wa_id]);
 
@@ -18,27 +19,10 @@ export default function ChatWindow({wa_id}) {
         <div className="w-2/3 flex flex-col bg-chat-pattern">
             <div className="flex-1 p-4 overflow-y-auto space-y-3">
                 {messages.map((msg, index) => {
-                    const showName = index === 0 || messages[index - 1].wa_id !== msg.wa_id;
-                    return (
-                        <div key={index}>
-                            {showName && (
-                                <div className="text-xs font-bold text-gray-600 mb-1">
-                                    {msg.name}
-                                </div>
-                            )}
-                            <MessageBubble key={index} message={msg} isSender={msg.name === 'You'} />
-                        </div>
-                    );
+                    <MessageBubble key={index} message={msg} isSender={msg.name === 'You'} />
                 })}
             </div>
-            <SendMessageForm conversation_id={wa_id} reload={() => fetchMessages(conversation_id).then(setMessages)}/>
+            <SendMessageForm wa_id={wa_id} reload={load}/>
         </div>
     );
-
 }
-
-
-
-
-
-
